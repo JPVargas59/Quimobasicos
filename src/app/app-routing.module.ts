6262593ee90d44f1e58a2fcaa0cfd3f8fa3ef50a
Juan Pablo Vargas Rodríguez <jpvargas59@gmail.com>
2020-03-31 17:54:28 -0600

    Started initial routes for project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {AdminHomeComponent} from './components/admin/admin-home/admin-home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', children: [
+      { path: '', component: AdminHomeComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'users/add', component: AddUsersComponent },
+      { path: 'user/:id', component: UserComponent },
+    ]},
+  /*{ path: 'supervisor', children: [
+      { path: '', component: SupervisorHomeComponent },
+      { path: 'inventory', component: InventoryComponent },
+      { path: 'tanks/add', component: AddTankComponent },
+      { path: 'tank/:id', component: TankComponent },
+      { path: 'tank/:id/update', component: UpdateTankComponent },
+      { path: 'tank/:id/exit', component: TankExitComponent },
+      { path: 'reports', component: ReportsComponent },
+    ]},
+  { path: 'operator', children: [
+      { path: '', component: OpreatorHomeComponent },
+      { path: 'inventory', component: InventoryComponent },
+      { path: 'tanks/add', component: AddTankComponent },
+      { path: 'tank/:id', component: TankComponent },
+      { path: 'tank/:id/update', component: UpdateTankComponent },
+      { path: 'tank/:id/exit', component: TankExitComponent },
+    ]},*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminHomeComponent } from './components/admin/admin-home/admin-home.component';
+import { UsersComponent } from './components/admin/users/users.component';
+import { AddUserComponent } from './components/admin/add-user/add-user.component';
+import { UserComponent } from './components/admin/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminHomeComponent,
+    UsersComponent,
+    AddUserComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
