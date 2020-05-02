8599aee9237950f36baba1455891ff79ef7ce296
davidlunadeleon <davidlunadeleon@gmail.com>
2020-05-02 18:36:53 +0000

    Add datetime scalar to the graphql schema.

--- Node_Back/src/resolvers/query.ts
+++ Node_Back/src/resolvers/query.ts
@@ -1,9 +1,11 @@
 import { IResolvers } from 'graphql-tools';
+import { GraphQLDateTime } from 'graphql-iso-date';
 const query: IResolvers = {
 	Query: {
 		hello(): string {
 			return 'HOLA MUNDO';
 		}
-	}
+	},
+	Date: GraphQLDateTime
 };
 export default query;
