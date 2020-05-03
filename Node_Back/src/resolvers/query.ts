b0b467df3294cbe9b1f3c10b8547bc9a14e2ed23
davidlunadeleon <davidlunadeleon@gmail.com>
2020-05-03 14:59:34 +0000

    Add scalars resolver to resolversMap
    Signed-off-by: davidlunadeleon <davidlunadeleon@gmail.com>

--- Node_Back/src/resolvers/query.ts
+++ Node_Back/src/resolvers/query.ts
@@ -1,11 +1,9 @@
 import { IResolvers } from 'graphql-tools';
-import { GraphQLDateTime } from 'graphql-iso-date';
 const query: IResolvers = {
 	Query: {
 		hello(): string {
 			return 'HOLA MUNDO';
 		}
-	},
-	Date: GraphQLDateTime
+	}
 };
 export default query;
--- Node_Back/src/resolvers/resolversMap.ts
+++ Node_Back/src/resolvers/resolversMap.ts
@@ -1,7 +1,9 @@
 import { IResolvers } from 'graphql-tools';
 import query from './query';
+import scalars from './scalars';
 
 const resolversMap: IResolvers = {
-	...query
+	...query,
+	...scalars
 };
 export default resolversMap;
