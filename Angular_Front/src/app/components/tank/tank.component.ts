505c442f376e86596bbe6f7988a75dfc9ce3abe3
Raul Castellanos <raulcastellanosh@hotmail.com>
2020-05-18 23:19:43 -0500

    visual changes

--- Angular_Front/src/app/components/tank/tank.component.ts
+++ Angular_Front/src/app/components/tank/tank.component.ts
@@ -15,6 +15,8 @@ export class TankComponent implements OnInit {
   updateAddress: string;
   exitAddress: string;
   tanks: [];
+  tankInfo: any;
+  tankWeight: any;
   
   constructor(
     route: ActivatedRoute,
@@ -29,7 +31,14 @@ export class TankComponent implements OnInit {
 
   // TODO: hacer llamada de getTank
   ngOnInit(): void {
-    
+    this.db.getTank(this.tank).subscribe(result => {
+      const tankInfo = result as any;
+      this.tankInfo = tankInfo.data.tanque;
+    })
+    this.db.getTankWeight(this.tank).subscribe(result => {
+      const tankWeight = result as any;
+      this.tankWeight = tankWeight.data.historialPesoTanque;
+      console.log(this.tankWeight)
+    })
   }
-
 }
--- Angular_Front/src/app/services/database.service.ts
+++ Angular_Front/src/app/services/database.service.ts
@@ -14,9 +14,18 @@ export class DatabaseService {
     private http: HttpClient
   ) { }
 
+  getTank(idTanque) {
+    const query = `{tanque(idTanque:"${idTanque}"){ contenidoTanque {contenido} dueno {nombre} calidad estadoValvula peso fechaIngreso fechaEsperadaRetorno observaciones}}`;
+    return this.http.post(`${this.homeURL}`,{query});
+  }
+
+  getTankWeight(idTanque){
+    const query = `{ historialPesoTanque (idTanque:"${idTanque}") { idTanque fecha peso}}`
+    return this.http.post(`${this.homeURL}`,{query});
+  }
+
   getTanks() {
-    
-    const json = '{tanques {idTanque idEtiqueta{idEtiqueta} fechaEsperadaRetorno peso contenidoTanque {contenido} lugar {lnombre}}}';
+    const json = '{tanques {idTanque contenidoTanque {contenido} fechaIngreso  fechaEsperadaRetorno  lugar {lnombre} peso  idEtiqueta{idEtiqueta}  observaciones}}';
     return this.http.get(`${this.homeURL}?query=${json}`);
   }
 
