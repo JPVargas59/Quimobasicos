c184a6a88bfcedad6b6486dd46d642d88a683acd
davidlunadeleon <davidlunadeleon@gmail.com>
2020-05-30 20:29:20 -0500

    GraphQL: Exigir token para todas las mutaciones  - Exigir token para todas las mutaciones menos hacer login

--- Node_Back/src/resolvers/mutation.ts
+++ Node_Back/src/resolvers/mutation.ts
@@ -3,178 +3,241 @@ import mysqlMutations = require('../data/asyncMySQLMutations');
 
 const mutation: IResolvers = {
 	Mutation: {
-		setTanque(__: void, { tanqueInput, idTanqueOriginal }): any {
-			return mysqlMutations.setValor(
-				tanqueInput,
-				[idTanqueOriginal],
-				'Tanque',
-				['idTanque']
-			);
+		setTanque(__: void, { tanqueInput, idTanqueOriginal }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(tanqueInput, [idTanqueOriginal], 'Tanque', [
+					'idTanque'
+				]);
+			}
 		},
-		createTanque(__: void, { tanqueInput }): any {
-			return mysqlMutations.createValor(tanqueInput, 'Tanque', [
-				'idTanque'
-			]);
+		createTanque(__: void, { tanqueInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(tanqueInput, 'Tanque', ['idTanque']);
+			}
 		},
-		deleteTanque(__: void, { idTanqueInput }): any {
-			return mysqlMutations.deleteTanque(idTanqueInput);
+		deleteTanque(__: void, { idTanqueInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.deleteTanque(idTanqueInput);
+			}
 		},
-		setLugar(__: void, { lugarInput, idLugarOriginal }): any {
-			return mysqlMutations.setValor(
-				lugarInput,
-				[idLugarOriginal],
-				'Lugar',
-				['idLugar']
-			);
+		setLugar(__: void, { lugarInput, idLugarOriginal }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(lugarInput, [idLugarOriginal], 'Lugar', ['idLugar']);
+			}
 		},
-		createLugar(__: void, { lugarInput }): any {
-			return mysqlMutations.createValor(lugarInput, 'Lugar', ['idLugar']);
+		createLugar(__: void, { lugarInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(lugarInput, 'Lugar', ['idLugar']);
+			}
 		},
-		setContenido(__: void, { contenidoInput, idContenidoOriginal }): any {
-			return mysqlMutations.setValor(
-				contenidoInput,
-				[idContenidoOriginal],
-				'Contenido',
-				['idContenido']
-			);
+		setContenido(__: void, { contenidoInput, idContenidoOriginal }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(contenidoInput, [idContenidoOriginal], 'Contenido', [
+					'idContenido'
+				]);
+			}
 		},
-		createContenido(__: void, { contenidoInput }): any {
-			return mysqlMutations.createValor(contenidoInput, 'Contenido', [
-				'idContenido'
-			]);
+		createContenido(__: void, { contenidoInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(contenidoInput, 'Contenido', ['idContenido']);
+			}
 		},
-		setDueno(__: void, { duenoInput, idDuenoOriginal }): any {
-			return mysqlMutations.setValor(
-				duenoInput,
-				[idDuenoOriginal],
-				'Dueno',
-				['idDueno']
-			);
+		setDueno(__: void, { duenoInput, idDuenoOriginal }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(duenoInput, [idDuenoOriginal], 'Dueno', ['idDueno']);
+			}
 		},
-		createDueno(__: void, { duenoInput }): any {
-			return mysqlMutations.createValor(duenoInput, 'Dueno', ['idDueno']);
+		createDueno(__: void, { duenoInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(duenoInput, 'Dueno', ['idDueno']);
+			}
 		},
 		setMantenimiento(
 			__: void,
-			{ mantenimientoInput, idMantenimientoOriginal }
+			{ mantenimientoInput, idMantenimientoOriginal },
+			{ token }
 		): any {
-			return mysqlMutations.setValor(
-				mantenimientoInput,
-				idMantenimientoOriginal,
-				'Mantenimiento',
-				['idTanque', 'fechaMantenimiento']
-			);
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(
+					mantenimientoInput,
+					idMantenimientoOriginal,
+					'Mantenimiento',
+					['idTanque', 'fechaMantenimiento']
+				);
+			}
 		},
-		createMantenimiento(__: void, { mantenimientoInput }): any {
-			return mysqlMutations.createValor(
-				mantenimientoInput,
-				'Mantenimiento',
-				['idTanque', 'fechaMantenimiento']
-			);
+		createMantenimiento(__: void, { mantenimientoInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(mantenimientoInput, 'Mantenimiento', [
+					'idTanque',
+					'fechaMantenimiento'
+				]);
+			}
 		},
-		setEtiqueta(__: void, { etiquetaInput, idEtiquetaOriginal }): any {
-			return mysqlMutations.setValor(
-				etiquetaInput,
-				idEtiquetaOriginal,
-				'EtiquetaRFID',
-				['idEtiqueta']
-			);
+		setEtiqueta(__: void, { etiquetaInput, idEtiquetaOriginal }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(etiquetaInput, idEtiquetaOriginal, 'EtiquetaRFID', [
+					'idEtiqueta'
+				]);
+			}
 		},
-		createEtiqueta(__: void, { etiquetaInput }): any {
-			return mysqlMutations.createValor(etiquetaInput, 'EtiquetaRFID', [
-				'idEtiqueta'
-			]);
+		createEtiqueta(__: void, { etiquetaInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(etiquetaInput, 'EtiquetaRFID', ['idEtiqueta']);
+			}
 		},
 		setHistorialPeso(
 			__: void,
-			{ historialPesoInput, idHistorialPesoOriginal }
+			{ historialPesoInput, idHistorialPesoOriginal },
+			{ token }
 		): any {
-			return mysqlMutations.setValor(
-				historialPesoInput,
-				idHistorialPesoOriginal,
-				'HistorialPeso',
-				['idTanque', 'fecha']
-			);
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(
+					historialPesoInput,
+					idHistorialPesoOriginal,
+					'HistorialPeso',
+					['idTanque', 'fecha']
+				);
+			}
 		},
-		createHistorialPeso(__: void, { historialPesoInput }): any {
-			return mysqlMutations.createValor(
-				historialPesoInput,
-				'HistorialPeso',
-				['idTanque', 'fecha']
-			);
+		createHistorialPeso(__: void, { historialPesoInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(historialPesoInput, 'HistorialPeso', [
+					'idTanque',
+					'fecha'
+				]);
+			}
 		},
-		setOperadoPor(
-			__: void,
-			{ operadoPorInput, idOperadoPorOriginal }
-		): any {
-			return mysqlMutations.setValor(
-				operadoPorInput,
-				idOperadoPorOriginal,
-				'OperadoPor',
-				['idTanque', 'fecha']
-			);
+		setOperadoPor(__: void, { operadoPorInput, idOperadoPorOriginal }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(
+					operadoPorInput,
+					idOperadoPorOriginal,
+					'OperadoPor',
+					['idTanque', 'fecha']
+				);
+			}
 		},
-		createOperadoPor(__: void, { operadoPorInput }): any {
-			return mysqlMutations.createValor(operadoPorInput, 'OperadoPor', [
-				'idTanque',
-				'fecha'
-			]);
+		createOperadoPor(__: void, { operadoPorInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(operadoPorInput, 'OperadoPor', [
+					'idTanque',
+					'fecha'
+				]);
+			}
 		},
-		setTanqueEsta(
-			__: void,
-			{ tanqueEstaInput, idTanqueEstaOriginal }
-		): any {
-			return mysqlMutations.setValor(
-				tanqueEstaInput,
-				idTanqueEstaOriginal,
-				'TanqueEsta',
-				['idTanque']
-			);
+		setTanqueEsta(__: void, { tanqueEstaInput, idTanqueEstaOriginal }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(
+					tanqueEstaInput,
+					idTanqueEstaOriginal,
+					'TanqueEsta',
+					['idTanque']
+				);
+			}
 		},
-		createTanqueEsta(__: void, { tanqueEstaInput }): any {
-			return mysqlMutations.createValor(tanqueEstaInput, 'TanqueEsta', [
-				'idTanque'
-			]);
+		createTanqueEsta(__: void, { tanqueEstaInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(tanqueEstaInput, 'TanqueEsta', ['idTanque']);
+			}
 		},
 		setTanqueHaEstado(
 			__: void,
-			{ tanqueHaEstadoInput, idTanqueHaEstadoOriginal }
+			{ tanqueHaEstadoInput, idTanqueHaEstadoOriginal },
+			{ token }
 		): any {
-			return mysqlMutations.setValor(
-				tanqueHaEstadoInput,
-				idTanqueHaEstadoOriginal,
-				'TanqueHaEstado',
-				['idTanque', 'idLugar', 'fecha']
-			);
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(
+					tanqueHaEstadoInput,
+					idTanqueHaEstadoOriginal,
+					'TanqueHaEstado',
+					['idTanque', 'idLugar', 'fecha']
+				);
+			}
 		},
-		createTanqueHaEstado(__: void, { tanqueHaEstadoInput }): any {
-			return mysqlMutations.createValor(
-				tanqueHaEstadoInput,
-				'TanqueHaEstado',
-				['idTanque', 'idLugar', 'fecha']
-			);
+		createTanqueHaEstado(__: void, { tanqueHaEstadoInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(tanqueHaEstadoInput, 'TanqueHaEstado', [
+					'idTanque',
+					'idLugar',
+					'fecha'
+				]);
+			}
 		},
-		setUsuario(__: void, { usuarioInput, idUsuarioOriginal }): any {
-			return mysqlMutations.setValor(
-				usuarioInput,
-				idUsuarioOriginal,
-				'Usuario',
-				['idUsuario']
-			);
+		setUsuario(__: void, { usuarioInput, idUsuarioOriginal }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.setValor(usuarioInput, idUsuarioOriginal, 'Usuario', [
+					'idUsuario'
+				]);
+			}
 		},
-		createUsuario(__: void, { usuarioInput }): any {
-			return mysqlMutations.createValor(usuarioInput, 'Usuario', [
-				'idUsuario'
-			]);
+		createUsuario(__: void, { usuarioInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.createValor(usuarioInput, 'Usuario', ['idUsuario']);
+			}
 		},
-		cambiarContrasena(__: void, { contrasenaInput }): any {
-			return mysqlMutations.cambiarContrasena(contrasenaInput);
+		cambiarContrasena(__: void, { contrasenaInput }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.cambiarContrasena(contrasenaInput);
+			}
 		},
 		login(__: void, { correo, contrasena }): any {
 			return mysqlMutations.login(correo, contrasena);
 		},
-		refresh(__: void, { refreshJWT }): any {
-			return mysqlMutations.refresh(refreshJWT);
+		refresh(__: void, { refreshJWT }, { token }): any {
+			if (!token) {
+				throw new Error('Acceso no autorizado');
+			} else {
+				return mysqlMutations.refresh(refreshJWT);
+			}
 		}
 	}
 };
