64941c7bb9443b101cbe1d1c32aec08e00b576ee
Raul Castellanos <raulcastellanosh@hotmail.com>
2020-06-09 14:33:45 -0500

    Se agrega etiquetas en modulo de operador

--- Angular_Front/src/app/modules/operator/operator.module.ts
+++ Angular_Front/src/app/modules/operator/operator.module.ts
@@ -7,6 +7,10 @@ import { AddTankComponent } from 'src/app/components/tank/add-tank/add-tank.comp
 import { TankComponent } from 'src/app/components/tank/tank.component';
 import { ExitTankComponent } from 'src/app/components/tank/exit-tank/exit-tank.component';
 import { OperatorLandingComponent } from 'src/app/components/operator/operator-landing/operator-landing.component';
+import { LectoresRfidComponent } from 'src/app/components/admin/lectores-rfid/lectores-rfid.component';
+import { AddLectorRfidComponent } from 'src/app/components/admin/lectores-rfid/add/add-lector-rfid.component';
+import { RfidTagsComponent } from 'src/app/components/admin/rfid-tags/rfid-tags.component';
+import { EditTagComponent } from 'src/app/components/admin/rfid-tags/edit-tag/edit-tag.component';
 
 const routes: Routes = [
   { path: '',
@@ -17,7 +21,14 @@ const routes: Routes = [
       { path: 'tanks/add', component: AddTankComponent },
       { path: 'tank/:id', component: TankComponent },
       { path: 'tank/:id/edit', component: AddTankComponent },
-      { path: 'tank/:id/exit', component: ExitTankComponent }
+      { path: 'tank/:id/exit', component: ExitTankComponent },
+      
+      { path: 'rfid-readers', component: LectoresRfidComponent },
+      { path: 'rfid-readers/add', component: AddLectorRfidComponent },
+      { path: 'rfid-readers/:readerId', component: AddLectorRfidComponent },
+      { path: 'rfid-tags', component: RfidTagsComponent },
+      { path: 'rfid-tags/add', component: EditTagComponent },
+      { path: 'rfid-tags/:tagId/edit', component: EditTagComponent },
     ]
   }
 ];
