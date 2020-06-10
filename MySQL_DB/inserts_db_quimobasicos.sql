dddb1a76a88e9ed4a720a02bc1b9c17a03691df6
davidlunadeleon <davidlunadeleon@gmail.com>
2020-06-10 18:06:27 -0500

    SQL: Actualizar base de datos y triggers

--- MySQL_DB/inserts_db_quimobasicos.sql
+++ MySQL_DB/inserts_db_quimobasicos.sql
@@ -1,4 +1,4 @@
-    USE quimobasicos;
+USE quimobasicos;
 
 INSERT INTO Contenido VALUES ("G-134A", "Cloro");
 INSERT INTO Contenido VALUES ("G-404A", "Hipoclorito de sodio");
@@ -64,22 +64,6 @@ INSERT INTO Lugar VALUES("PT2", "Patio secundario",665.3,2000,ST_GeomFromText('P
 INSERT INTO Lugar VALUES("ETDA2", "Entrada Anexo 2",56.573,2980,ST_GeomFromText('POINT (44.53456  55.53242)'));
 INSERT INTO Lugar VALUES("Zo1", "Zona 1",5.698,3000,ST_GeomFromText('POINT (90.53456  20.53242)'));
 INSERT INTO Lugar VALUES("ETDA3", "Entrada Trasera",5.6987,2698,ST_GeomFromText('POINT (15.4578  98.3651)'));
-
-INSERT INTO TanqueEsta VALUES (
-	"NVVU100706", "PT","2020-10-01"
-);
-INSERT INTO TanqueEsta VALUES (
-	"EURO514966", "AMC", "2020-10-01"
-);
-INSERT INTO TanqueEsta VALUES (
-	"EURO514967", "ETD", "2020-10-02"
-);
-INSERT INTO TanqueEsta VALUES (
-	"NVVU100711", "ETDA2", "2020-10-02"
-);
-INSERT INTO TanqueEsta VALUES (
-	"EURO514970", "Zo1", "2020-10-02"
-);
 
 INSERT INTO TanqueHaEstado VALUES (
 	"NVVU100706", "PT", "2020-02-08 10:20:00"
